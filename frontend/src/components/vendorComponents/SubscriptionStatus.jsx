f2d9748cf8e551be3f1d0d446724cb386e960455
ram220 <vallururamakrishna9@gmail.com>
2026-04-27 22:56:01 +0530

    change in subscription

--- frontend/src/components/vendorComponents/SubscriptionStatus.jsx
+++ frontend/src/components/vendorComponents/SubscriptionStatus.jsx
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import API_URL from "../../config";
 
 function SubscriptionStatus() {
 
   const [subscription,setSubscription]=useState(null);
-
-  //const API_URL="https://localbasket-multi-vendor-marketplace.onrender.com";
-
-  const API_URL="http://localhost:8000";
 
   const token=localStorage.getItem("token");
   useEffect(()=>{
